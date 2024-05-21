9cfe6584a51f458938c9826a49fa2e90ed96779f
harryTT.Ju <harryTT_Ju@moxa.com>
2024-05-21 17:20:29 +0800

    feat(add task queue module)&fix(1. http code comment 2. clean main.go )

--- http/http.go
+++ http/http.go
@@ -7,18 +7,17 @@ import (
 )
 
 func SendGETRequest(uri string) (*http.Response, error){
-	// 创建一个新的 HTTP 请求
+	// Create http request
 	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return nil, err
 	}
 
-	// 设置请求头
+	// set header
 	req.Header.Set("Accept", "vdn.dac.v1")
 	req.Header.Set("Content-Type", "application/json")
 
-	// 使用默认的 HTTP 客户端发送请求
 	client := http.DefaultClient
 	resp, err := client.Do(req)
 	if err != nil {
@@ -30,18 +29,16 @@ func SendGETRequest(uri string) (*http.Response, error){
 }
 
 func SendPUTRequest(uri string, jsonBody string) (*http.Response, error) {
-    // 创建一个新的 HTTP 请求
+    // Create http request
     req, err := http.NewRequest("PUT", uri, strings.NewReader(jsonBody))
     if err != nil {
         fmt.Println("Error creating request:", err)
         return nil, err
     }
 
-    // 设置请求头
     req.Header.Set("Accept", "vdn.dac.v1")
     req.Header.Set("Content-Type", "application/json")
 
-    // 使用默认的 HTTP 客户端发送请求
     client := http.DefaultClient
     resp, err := client.Do(req)
     if err != nil {
