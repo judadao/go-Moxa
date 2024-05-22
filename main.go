78efc373872be97900baeb00d12e2a4bfb04a068
harryTT.Ju <harryTT_Ju@moxa.com>
2024-05-22 16:54:18 +0800

    feat(1. add different device dynamic create device obj 2.add device name 3. add muti condition check func) fix(rm obj interface)

--- main.go
+++ main.go
@@ -16,17 +16,22 @@ func main() {
 
 	taskQueue := task_qu.NewTaskQueue()
 
-	input := "device{(e1200, e1213, 192.168.127.254)}, when{[(do,0)=0]}, then{[(do,1)=1]&&[(do,2)=1]&&[(do,3)=1]&&[(do,4)=0]&&[(do,5)=0]&&[(do,6)=0]}"
+
+	input2 := "device{[(e1200, e1213, 192.168.127.254)=device1]&&[(e1200, e1213, 192.168.127.254)=device2]}, when{[(device1, do,0)=1]&&[(device2, do,1)=1]}, then{[(device1,do,2)=0]&&[(device1, do,3)=0]}"
+	extracted2 := task_qu.ExtractContent(input2)
+
+	input := "device{[(e1200, e1213, 192.168.127.254)=device1]&&[(e1200, e1213, 192.168.127.254)=device2]}, when{[(device1, do,0)=1]&&[(device2, do,1)=0]}, then{[(device1,do,2)=1]&&[(device1, do,3)=1]}"
 	extracted := task_qu.ExtractContent(input)
 
-	input2 := "device{(e1200, e1213, 192.168.127.254)}, when{[(do,0)=1]}, then{[(do,1)=0]&&[(do,2)=0]&&[(do,3)=0]&&[(do,4)=1]&&[(do,5)=1]&&[(do,6)=1]}"
-	extracted2 := task_qu.ExtractContent(input2)
+	
+
+
+	taskQueue.AddTask(func(taskInfo task_qu.Task) {
+		allTask.Task_func(&extracted2)
+	})
 
 	taskQueue.AddTask(func(taskInfo task_qu.Task) {
 		allTask.Task_func(&extracted)
-	})
-	taskQueue.AddTask(func(taskInfo task_qu.Task) {
-		allTask.Task_func(&extracted2)
 	})
 	go func() {
 		for {
